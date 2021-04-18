5ab7342fcf65918929b8f9c746cc10edb98e6a1f
lixj-zj <lixj_zj@163.com>
2021-04-18 21:32:27 +0800

    多线程-生产者消费者；

---
+++ thread/demo/TestProducerConsumer2.java
@@ -0,0 +1,12 @@
+/**
+ * @Description: --------------------------------------
+ * @ClassName: TestProducerConsumer2.java
+ * @Date: 2021/4/18 16:35
+ * @SoftWare: IntelliJ IDEA
+ * --------------------------------------
+ * @Author: lixj
+ * @Contact: lixj_zj@163.com
+ **/
+public class TestProducerConsumer2 {
+
+}
